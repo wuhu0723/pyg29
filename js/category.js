112f03dc5c6752d6a9e849504dbe39385491f284
wuhu <wuhu@itcast.cn>
2019-04-04 10:08:04 +0800

    11-分类页面静态结构完成

--- js/category.js
+++ js/category.js
@@ -1,8 +1,11 @@
 $(function(){
+    // 初始化iscroll
     var myScroll = new IScroll('.left');
-
+    // 为左侧li绑定单击操作
     $('.left').on('tap','li',function(){
+        // 样式的切换
         $(this).addClass('active').siblings().removeClass('active')
+        // 实现 元素置顶
         myScroll.scrollToElement(this)
     })
 })
