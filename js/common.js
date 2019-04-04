85bd6f54a21305b959ffacf2d6fb473409b73534
wuhu <wuhu@itcast.cn>
2019-04-04 09:13:39 +0800

    09-解决mui不响应a单击的问题

--- js/common.js
+++ js/common.js
@@ -1,4 +1,12 @@
 $(function(){
+    // 默认情况 下，mui不响应click单击事件，这是它的默认行为
+    // 我们解决方式就是重新为所有A绑定tap
+    mui('body').on('tap', 'a', function (e) {
+        e.preventDefault()
+        window.top.location.href = this.href;
+    });
+
+
     const baseURL = 'http://157.122.54.189:9094/api/public/v1/'
     // 添加zepto拦截器：它的作用是可以让每个ajax请求都经过这个函数进行处理
     // beforeSend：每次发送ajax请求都必须经过的处理函数
